ab5bab2b077efcd015b87e462eaa5e7646fea7c8
Demehin Ibukunoluwa George <ibk2k7@gmail.com>
2020-09-28 14:38:57 +0100

    slight change to google credentials lines 159 settings.py

--- parfaitweb/settings.py
+++ parfaitweb/settings.py
@@ -156,7 +156,7 @@ EMAIL_HOST_PASSWORD = os.environ['DB_PASS']
 from google.oauth2 import service_account
 import json
 service_account_info = json.load(open(os.environ['GOOGLE_APPLICATION_CREDENTIALS']))
-GS_CREDENTIALS = service_account.Credentials.from_service_account_file(
+GS_CREDENTIALS = service_account.Credentials.from_service_account_info(
     service_account_info
 )
 
